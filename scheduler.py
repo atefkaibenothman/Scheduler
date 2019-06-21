13218ca814176e4bb27d2309d585d3e38eec6814
Atef Kai Benothman <abenothm@uci.edu>
2019-06-21 00:18:28 -0700

    Added to-do and other minor changes

--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,12 @@
+# scheduler.py
+# Atef Kai Benothman 6/20/2019
+
+# To-Do:
+# 1. Refactor code
+# 2. Implement dictionary
+
+
+from collections import defaultdict
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import re
@@ -25,13 +34,17 @@ try:
 
         lt.append(l)
 
+    d = defaultdict()
     with open("course_list.txt", "w") as f:
         for course in lt:
             if (len(course) == 1):
+                d[course[0]] = {}
                 f.write(course[0])
 
                 if course[0] != "WRITING":
                     f.write("\n")
+
+    print(d)
 
 finally:
     driver.quit()
